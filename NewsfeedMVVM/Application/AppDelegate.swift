9112556e7a402e404e9ebe5e00be9d169d292cd9
Alexander Milgunov <nagvalbox@bk.ru>
2021-02-09 13:23:54 +0500

    di network

--- NewsfeedMVVM/Application/AppDelegate.swift
+++ NewsfeedMVVM/Application/AppDelegate.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import Swinject
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    let container: Container = {
+            let container = Container()
+            container.register(NetworkManager.self) { _ in NetworkManager() }
+            return container
+        }()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
